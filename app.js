e6c24ccacc3300248aeb2d97b59f12cc8782e99b
pad19 <pad19051999@gmail.com>
2020-02-16 19:59:23 +0200

    commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const views = require('./views');
+const stuff = require('./data');
+const fetch = require('node-fetch');
+const app = express();
+
+app.get('/api', (req, res) => {
+
+    const perPage = 'perPage' in req.query && Number(req.query.perPage) ? Number(req.query.perPage) : 3;
+    const currentPage = 'page' in req.query && Number(req.query.page) ? Number(req.query.page) : 1;
+    const urlPrefix = '/api?page=';
+    const pages = Math.ceil(stuff.length/perPage);
+
+    res.json({
+        info: {
+            count: stuff.length,
+            pages,
+            next: getNextPageUrl(currentPage, pages, urlPrefix),
+            prev: getPrevPageUrl(currentPage, urlPrefix),
+            currentPage,
+            perPage,
+        },
+        results: stuff.slice((currentPage-1)*perPage,currentPage*perPage)
+    });
+});
+app.get('/', (req, res) => {
+    req.query.page ? 
+        fetch(`http://localhost:3000/api?page=${req.query.page}`)
+            .then(result => result.json())
+            .then(result => {
+                const {results} = result;
+                const html = '<tbody>' + results.map(el => '<tr>' + Object.values(el).map(elem => `<td>${elem}</td>`).join('') + '</tr>').join('') + '</tbody>';
+                res.send(views(html));
+            })
+        : res.send(views(''));
+});
+
+const getNextPageUrl = (current_page, total_pages, url_prefix) => current_page < total_pages ? `${url_prefix}${current_page+1}` : null;
+const getPrevPageUrl = (current_page, url_prefix) => current_page > 1 ? `${url_prefix}${current_page-1}` : null;
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Server was runnig...');
+});
---
+++ data/index.js
@@ -0,0 +1,12 @@
+module.exports = stuff = [
+    { id: 1, fullName: 'Oleh Lev', position: 'Web Dev', skill: 'PHP,JS', exp: 3, sex: 'Male', salary: 4500 },
+    { id: 2, fullName: 'John White', position: 'Web Dev', skill: 'PHP', exp: 1, sex: 'Male', salary: 1200 },
+    { id: 3, fullName: 'Jany Rad', position: 'Sale', skill: '-', exp: 2, sex: 'Famale', salary: 3500 },
+    { id: 4, fullName: 'Ivan Brown', position: 'iOS', skill: 'Swift', exp: 3, sex: 'Male', salary: 4000 },
+    { id: 5, fullName: 'Pet Bool', position: 'Android', skill: 'Java', exp: 2, sex: 'Male', salary: 3520 },
+    { id: 6, fullName: 'Emma Hallo', position: 'Android', skill: 'Kotlin', exp: 1, sex: 'Famale', salary: 2520 },
+    { id: 7, fullName: 'Olivia Jones', position: 'iOS', skill: 'Objective-C', exp: 3, sex: 'Famale', salary: 2820 },
+    { id: 8, fullName: 'William Smith', position: 'Designer', skill: '-', exp: 5, sex: 'Male', salary: 3000 },
+    { id: 9, fullName: 'Oliver Alien', position: 'PM', skill: '-', exp: 4, sex: 'Male', salary: 6000 },
+    { id: 10, fullName: 'Mia Morris', position: 'Owner', skill: '-', exp: 10, sex: 'Famale', salary: 10000 },
+];
